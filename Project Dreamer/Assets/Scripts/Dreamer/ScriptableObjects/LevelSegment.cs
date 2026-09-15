72524173e7371f13a8134458f9f7d0b10aa6f58e
probablybees <JRissberger@gmail.com>
2026-09-15 13:55:47 -0400

    Setup notes

--- Project Dreamer/Assets/Scripts/Dreamer/ScriptableObjects/LevelSegment.cs
+++ Project Dreamer/Assets/Scripts/Dreamer/ScriptableObjects/LevelSegment.cs
@@ -12,4 +12,10 @@ public class LevelSegment : ScriptableObject
 
     //Is this level segment included in wander pathing
     private bool isActive;
+
+
+    /* Manager -- holds list of class objects, serialize fields to add in values in editor
+     * Then adds all those to the scriptable object at runtime--scriptable object holds a list of these objects.
+     * Might be simpler if there'sa way to define/add the class to the inspector--check this!!
+     */
 }
